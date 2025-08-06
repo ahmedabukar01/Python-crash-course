44b9cb1b811137e2e2a7781ed3cdaf8ef47136be
ahmed <ahmedlearn01@gmail.com>
2025-08-06 12:20:11 +0300

    :rocket::rocket: Graphs

--- graphs.py
+++ graphs.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt # to short the long name
+import emoji
 
-# data
-x = [i for i in range(0,11)]
-y = [y ** 2 for y in x]
+# # data
+# x = [i for i in range(0,11)]
+# y = [y ** 2 for y in x]
 
-print(x)
-print(y)
+# print(x)
+# print(y)
 
+# plt.plot(x,y)
+# plt.xlabel('x axis')
+# plt.ylabel('y axis')
+# plt.show()
+
+
+print(emoji.emojize(':red_heart:'))
